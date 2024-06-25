ad3c16c86950264e41832e179e27013b2cffb94c
Lilian <lilianmon57@gmail.com>
2024-06-26 01:11:21 +0200

    update

--- exam_rank_2.c
+++ exam_rank_2.c
@@ -6,7 +6,11 @@
 /*   By: lmonsat <lmonsat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 16:16:35 by lmonsat           #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2024/05/30 19:47:32 by lmonsat          ###   ########.fr       */
+=======
+/*   Updated: 2024/06/26 01:06:48 by lmonsat          ###   ########.fr       */
+>>>>>>> 4c6cd70 (update)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +21,21 @@
 #include <unistd.h>
 #include <string.h>
 
+<<<<<<< HEAD
 /* ---- Level 1 ---- */
+=======
+/* <----------------- Level 1 -----------------> */
+
+>>>>>>> 4c6cd70 (update)
 #pragma region Level_1
 
 void ft_putnbr(int n)
 {
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        n = -n;
+    }
     if (n > 9)
     {
         ft_putnbr(n / 10);
@@ -142,6 +156,13 @@ void ulstr(int argc, char *argv[])
 }
 
 #pragma endregion
+<<<<<<< HEAD
+=======
+
+/* <----------------- Level 2 -----------------> */
+
+#pragma region Level_2
+>>>>>>> 4c6cd70 (update)
 
 /* ---- Level 2 ---- */
 #pragma region Level_2
@@ -401,11 +422,12 @@ char *ft_strrev(char *str)
     {
         len++;
     }
-    while (i < len - 1)
+    len--;
+    while (i < len)
     {
         temp = str[i];
-        str[i] = str[len - 1];
-        str[len - 1] = temp;
+        str[i] = str[len];
+        str[len] = temp;
         len--;
         i++;
     }
@@ -591,7 +613,7 @@ void ft_inter(int argc, char *argv[])
 				{
 					lookup[(int)argv[1][i]] = 1;
 					write(1, &argv[1][i], 1);
-					break;
+					//break;
 				}
 				j++;
 			}
@@ -603,8 +625,66 @@ void ft_inter(int argc, char *argv[])
 
 #pragma endregion
 
+<<<<<<< HEAD
 /* ---- Level 3 ---- */
 #pragma region Level_3
+=======
+/* <----------------- Level 3 -----------------> */
+
+#pragma region Level_3
+
+// Explication https://chatgpt.com/share/cfdef1fc-a1f3-4f72-8024-ff5fbdfc429f
+
+int is_prime(int nbr)
+{
+    // Si le nombre est inférieur à 2, il n'est pas premier
+    if (nbr < 2)
+        return 0;
+
+    int i = 2;
+
+    // Parcourt tous les entiers de 2 à nbr/2
+    while (i <= nbr / 2)
+    {
+        // Si nbr est divisible par i, alors nbr n'est pas premier
+        if (nbr % i == 0)
+            return 0; // Retourne 0 pour indiquer que nbr n'est pas premier
+        i++;
+    }
+
+    // Si aucun diviseur n'a été trouvé, nbr est premier
+    return 1; // Retourne 1 pour indiquer que nbr est premier
+}
+
+void add_prime_sum(int argc, char *argv[])
+{
+    // Vérifie que le nombre d'arguments est égal à 2 et que le deuxième argument est un entier positif
+    if (argc != 2 || ft_atoi(argv[1]) <= 0)
+    {
+        // Si les conditions ne sont pas remplies, affiche "0" suivi d'un saut de ligne et quitte la fonction
+        write(1, "0\n", 2);
+        return;
+    }
+
+    // Convertit le deuxième argument en entier
+    int n = ft_atoi(argv[1]);
+    int sum = 0; // Initialise la variable sum à 0 pour stocker la somme des nombres premiers
+
+    // Parcourt tous les entiers de n à 1
+    while (n > 1)
+    {
+        // Vérifie si n est un nombre premier en appelant la fonction is_prime
+        if (is_prime(n))
+            sum += n; // Ajoute n à la somme si n est premier
+        n--; // Décrémente n pour passer à l'entier précédent
+    }
+
+    // Affiche la somme calculée suivie d'un saut de ligne
+    ft_putnbr(sum);
+    write(1, "\n", 1);
+}
+
+>>>>>>> 4c6cd70 (update)
 
 int is_space(char c)
 {
@@ -710,14 +790,13 @@ int *ft_range(int start, int end)
         {
             res[i] = start;
             start++;
-            i++;
         }
         else
         {
             res[i] = start;
             start--;
-            i++;
         }
+        i++;
     }
     return (res);
 }
@@ -734,14 +813,13 @@ int *ft_rrange(int start, int end)
         {
             res[i] = end;
             end++;
-            i++;
         }
         else
         {
             res[i] = end;
             end--;
-            i++;
         }
+        i++;
     }
     return (res);
 }
@@ -830,43 +908,10 @@ void pgcd(int argc, char*argv[])
                     number2 -= number1;
                 }
             }
-        printf("%d", number1);
+            printf("%d", number1);
         }
     }
     printf("\n");
-}
-
-int ft_atoi2(const char *str)
-{
-    int i = 0;
-    int is_negative = 0;
-    int n = 0;
-
-    while (str[i] == " ")
-    {
-        i++;
-    }
-    if (str[i] == "-")
-    {
-        i++;
-        is_negative = 1;
-    }
-    else if (str[i] == "+")
-    {
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        n = n * 10 + str[i++] - '0';
-    }
-    if (is_negative)
-    {
-        return (-n);
-    }
-    else
-    {
-        return (n);
-    }
 }
 
 void put_hex(int n)
@@ -883,7 +928,7 @@ void print_hex(int argc, char*argv[])
 {
     if (argc == 2)
     {
-        put_hex(ft_atoi2(argv[1]));
+        put_hex(ft_atoi(argv[1]));
     }
     write(1, "\n", 1);
 }
@@ -981,7 +1026,7 @@ void tab_mult(int argc, char *argv[])
     }
 
     int i = 1;
-    int n = ft_atoi2(argv[1]);
+    int n = ft_atoi(argv[1]);
 
     while (i < 10)
     {
@@ -997,7 +1042,12 @@ void tab_mult(int argc, char *argv[])
 
 #pragma endregion
 
+<<<<<<< HEAD
 /* ---- Level 4 ---- */
+=======
+/* <----------------- Level 4 -----------------> */
+
+>>>>>>> 4c6cd70 (update)
 #pragma region Level_4
 
 typedef struct  s_point
@@ -1006,6 +1056,7 @@ int           x;
 int           y;
 }               t_point;
 
+<<<<<<< HEAD
 void fill(char **tab, t_point size, t_point cur, char to_fill)
 {
 	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
@@ -1021,33 +1072,169 @@ void flood_fill(char **tab, t_point size, t_point begin)
 {
     fill(tab, size, begin,tab[begin.x][begin.y]);
 }
+=======
+void fill(char **tab, t_point size, char target, int row, int col)
+{
+    if (row < 0 || col < 0 || row >= size.y || col >= size.x)
+        return ;
+
+    if (tab[row][col] == 'F' || tab[row][col] != target)
+        return ;
+
+    tab[row][col] = 'F';
+
+    fill(tab, size, target, row - 1, col);
+    fill(tab, size, target, row + 1, col);
+    fill(tab, size, target, row, col - 1);
+    fill(tab, size, target, row, col + 1);
+}
+
+void flood_fill(char **tab, t_point size, t_point begin)
+{
+    char target = tab[begin.y][begin.x];
+    fill(tab, size, target, begin.y, begin.x);
+}
+char** make_area(char** zone, t_point size)
+{
+	char** new;
+
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+
+	return new;
+}
+
+char *ft_itoa(int nbr)
+{
+    if (nbr == -2147483648)
+        return("-2147483648\0");
+
+    // Calcul de la longueur nécessaire pour la chaîne
+    
+    int n = nbr;
+    int len = 0;
+    if (nbr <= 0)
+        len++;
+    while (n)
+    {
+        n /= 10;
+        len++;
+    }
+    // Allocation
+    char *result = (char *)malloc(sizeof(char) * (len + 1));
+    if (result == NULL)
+        return NULL;
+    result[len] = '\0';
+
+    // Gestion des cas particuliers pour zéro et les nombres négatifs
+    
+    if (nbr == 0)
+    {
+        result[0] = '0';
+        return(result);
+    }
+    if (nbr < 0)
+    {
+        result[0] = '-';
+        nbr = -nbr;
+    }
+    
+    // Conversion des chiffres en caractères et remplissage de la chaîne
+
+    while (nbr)
+    {
+        result[--len] = nbr % 10 + '0';
+        nbr /= 10;
+    }
+    return (result);
+}
+
+// Explications https://chatgpt.com/share/b49475d5-3a65-47d6-9ee4-28a36231ae4e
+
+void ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    while (begin_list)
+    {
+        (*f)(begin_list->data);
+        begin_list = begin_list->next;
+    }
+}
+
+// Explications https://chatgpt.com/share/4c3b7128-caf4-44dc-ab80-fca0c40de361
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+    // Si le pointeur vers le début de la liste est NULL ou si la liste est vide, on ne fait rien.
+    if (begin_list == NULL || *begin_list == NULL)
+        return ;
+
+    // Déclaration d'un pointeur cur pour parcourir la liste.
+    t_list *cur = *begin_list;
+
+    // Si la donnée du premier élément de la liste satisfait la condition spécifiée par cmp.
+    if ((cmp)(cur->data, data_ref) == 0)
+    {
+        // On met à jour le début de la liste pour qu'il pointe vers le deuxième élément.
+        *begin_list = cur->next;
+        // On libère la mémoire de l'élément à supprimer.
+        free(cur);
+        // On appelle récursivement la fonction pour traiter le reste de la liste.
+        ft_list_remove_if(begin_list, data_ref, cmp);
+    }
+    else
+    {
+        // Sinon, on avance dans la liste et on appelle récursivement la fonction sur l'élément suivant.
+        cur = *begin_list;
+        ft_list_remove_if(&cur->next, data_ref, cmp);
+    }
+}
+
+// Explication https://chatgpt.com/share/00eb0c3e-cd8f-4fe4-b41a-7da6a4cb50ef
+>>>>>>> 4c6cd70 (update)
 
 void f_prime(int argc, char *argv[])
 {
+    // Vérifie s'il y a exactement deux arguments (le nom du programme et un nombre)
     if (argc == 2)
     {
-        int i = 0;
-        int number = argv[1]; 
+        int i = 2; // Initialisation du diviseur à 2 (le plus petit nombre premier)
+        int number = atoi(argv[1]); // Convertit le deuxième argument en entier
+        
+        // Si le nombre est 1, on imprime "1"
         if (number == 1)
         {
             printf("1");
         }
-        while (number >= i++)
+
+        // Boucle pour trouver et imprimer les facteurs premiers du nombre
+        while (number >= i)
         {
+            // Si i est un facteur de number
             if (number % i == 0)
             {
-                printf("%d", i);
+                printf("%d", i); // Imprime le facteur
+
+                // Si le nombre actuel est égal à i, il n'y a plus de facteurs à trouver
                 if (number == i)
                 {
-                    break ;
+                    break; // On sort de la boucle
                 }
-                printf("*");
-				number /= i;
-				i = 1;
+
+                printf("*"); // Imprime l'astérisque pour la séparation des facteurs
+                number /= i; // Divise number par i pour continuer la factorisation
+                i = 1; // Réinitialise i à 1 pour recommencer la boucle avec i = 2 au prochain incrément
             }
+            i++; // Incrémente i pour tester le prochain potentiel facteur
         }
     }
-    printf("\n");
+
+    printf("\n"); // Imprime une nouvelle ligne à la fin
 }
 
 void sort_int_tab(int *tab, unsigned int size)
@@ -1057,7 +1244,6 @@ void sort_int_tab(int *tab, unsigned int size)
 
     while (i < (size - 1))
     {
-
         if (tab[i] > tab[i + 1])
         {
             temp = tab[i];
@@ -1071,8 +1257,220 @@ void sort_int_tab(int *tab, unsigned int size)
     }
 }
 
+<<<<<<< HEAD
 #pragma endregion
 
+=======
+// Explications https://chatgpt.com/share/0eb901b3-df32-4b9e-a771-608ed02050ec
+
+char *ft_strncpy(char *dest, const char *src, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (src[i] && i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return dest;
+}
+
+char **ft_split(char *str)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int wc = 0;
+
+    // Compter le nombre de mots
+    while (str[i])
+    {
+        // Ignorer les espaces, tabulations et retours à la ligne
+        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+            i++;
+
+        // Si on trouve un caractère non-espace, c'est un début de mot
+        if (str[i])
+            wc++;
+
+        // Passer au mot suivant
+        while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+            i++;
+    }
+
+    // Allouer de la mémoire pour les pointeurs de mots (wc + 1 pour le NULL final)
+    char **out = (char **)malloc(sizeof(char *) * (wc + 1));
+    i = 0;
+
+    // Extraire chaque mot
+    while (str[i])
+    {
+        // Ignorer les espaces, tabulations et retours à la ligne
+        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+            i++;
+
+        // Début du mot
+        j = i;
+
+        // Trouver la fin du mot
+        while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+            i++;
+        
+        // Si on a trouvé un mot
+        if (i > j)
+        {
+            // Allouer de la mémoire pour le mot
+            out[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
+
+            // Copier le mot dans l'emplacement alloué
+            ft_strncpy(out[k++], &str[j], i - j);
+        }
+    }
+    // Terminer la liste des mots par un pointeur NULL
+    out[k] = NULL;
+    return (out);
+}
+
+// Explication https://chatgpt.com/share/03835be0-618b-4dc7-9180-a663d8b4a183
+
+void rev_wstr(int argc, char *argv[])
+{
+    int start;
+    int end;
+    int i = 0;
+
+    if (argc == 2)
+    {
+        // Calcul de la longueur de la chaîne
+        while (argv[1][i] != '\0')
+            i++;
+
+        // Parcourir la chaîne à l'envers
+        while (i >= 0)
+        {
+            // Ignorer les espaces et tabulations
+            while (i >= 0 && (argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t'))
+                i--;
+
+            if (i < 0) 
+                break;  // Sortir si début de chaîne atteint
+
+            // Fin du mot
+            end = i;
+
+            // Trouver le début du mot
+            while (i >= 0 && argv[1][i] != ' ' && argv[1][i] != '\t')
+                i--;
+
+            start = i + 1;
+
+            // Afficher le mot
+            int flag = start;
+            while (start <= end)
+            {
+                write(1, &argv[1][start], 1);
+                start++;
+            }
+
+            // Afficher un espace sauf après le dernier mot
+            if (flag != 0)
+            {
+                write(1, " ", 1);
+            }
+        }
+    }
+    write(1, "\n", 1);
+}
+
+void write_word(char *start, char*end)
+{
+    while (start < end)
+    {
+        write(1, start, 1);
+        start++;
+    }
+    
+}
+
+void rostring(int argc, char *argv[])
+{
+    char *str;
+    char *first_word_start;
+    char *first_word_end;
+
+    if (argc > 1)
+    {
+        str = argv[1];
+        while(*str == ' ' || *str == '\t')
+            str++;
+        first_word_start = str;
+        while (*str && *str != ' ' || *str != '\t')
+            str++;
+        first_word_end = str;
+        if (*str)
+        {
+            while (*str)
+            {
+                if (*str == ' ' || *str == '\t')
+                {
+                    while (*str == ' ' || *str == '\t')
+                        str++;
+                    if (*str)
+                        write(1, " ", 1);
+                }
+                else
+                {
+                    write(1, str, 1);
+                    str++;
+                }
+            }
+            write(1, " ", 1);
+        }
+        write_word(first_word_start, first_word_end);
+    }
+    write(1, '\n', 1);
+}
+
+// Explication https://chatgpt.com/share/7e4dd72a-0843-4dbb-97f9-13ecc310d229
+
+t_list *sort_list(t_list* lst, int (*cmp)(int, int)) 
+{
+    int swap;       // Variable pour échanger les valeurs
+    t_list *tmp;    // Pointeur temporaire pour marquer le début de la liste
+
+    tmp = lst;      // Initialiser tmp au début de la liste
+    while (lst->next != NULL) 
+    {  // Parcourir la liste jusqu'à l'avant-dernier élément
+        // Comparer l'élément courant avec l'élément suivant en utilisant la fonction cmp
+        if (((*cmp)(lst->data, lst->next->data)) == 0) 
+        {
+            // Si cmp retourne 0, échanger les valeurs des deux éléments
+            swap = lst->data;
+            lst->data = lst->next->data;
+            lst->next->data = swap;
+            lst = tmp;  // Revenir au début de la liste après un échange
+        } 
+        else 
+        {
+            // Si cmp ne retourne pas 0, avancer à l'élément suivant
+            lst = lst->next;
+        }
+    }
+    lst = tmp;  // Réinitialiser lst au début de la liste
+    return (lst);  // Retourner le pointeur vers le début de la liste triée
+}
+
+
+#pragma endregion
+
+
+>>>>>>> 4c6cd70 (update)
 int main (int argc, char *argv[])
 {
     //do_op(argc, argv);
@@ -1125,4 +1523,26 @@ int main (int argc, char *argv[])
     //exe_str_capitalizer(argc, argv);
 
     //tab_mult(argc, argv);
+
+	/*t_point size = {8, 5};
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+
+	char**  area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+
+	t_point begin = {7, 4};
+	flood_fill(area, size, begin);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);*/
+
+    //printf("test itoa %s\n", ft_itoa(-485236));
+
 }

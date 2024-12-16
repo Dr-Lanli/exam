823ed611091e6fefe16b548e08a51f8cf8387b6c
Lilian Monsat <lmonsat@c2r3s4.42luxembourg.lu>
2024-12-16 22:33:36 +0100

    update

--- exam_rank_3.c
+++ exam_rank_3.c
@@ -4,45 +4,40 @@
 int ft_putnbr(int n)
 {
 	int len = 0;
-
-    if (n < 0)
-    {
-        write(1, "-", 1);
-        n = -n;
-    }
-    if (n > 9)
-    {
-        ft_putnbr(n / 10);
-    }
+	if (n < 0)
+	{
+		write(1, &"-", 1);
+		n = -n;
+	}
+	if (n > 9)
+	{
+		len += ft_putnbr(n / 10);
+	}
+	write(1, &"0123456789"[n % 10], 1);
 	len++;
-    write(1, &"0123456789"[n % 10], 1);
 	return (len);
 }
 
 int ft_puthex(int n)
 {
-	static int len = 0;
-    if (n >= 16)
-    {
-		len++;
-        ft_putnbr(n / 16);
-    }
-	write(1, &"0", 1);
-    write(1, &"0123456789abcdef"[n % 16], 1);
-	len +=2;
+	int len = 0;
+
+	if (n >= 16)
+	{
+		len += ft_puthex(n / 16);
+	}
+	write(1, &"0123456789abcdef"[n % 16], 1);
+	len++;
 	return (len);
 }
 
 int ft_putstr(char *str)
 {
 	int len = 0;
-	if (!str)
-	{
-		return (0);
-	}
+
 	while (str && *str)
 	{
-		write(1, str++, 1);
+		str++;
 		len++;
 	}
 	return (len);
@@ -50,7 +45,7 @@ int ft_putstr(char *str)
 
 int spec_char(va_list args, const char *str)
 {
-	int len;
+	int len = 0;
 
 	if (*str == 'd')
 	{
@@ -69,9 +64,8 @@ int spec_char(va_list args, const char *str)
 
 int ft_printf(const char *str, ...)
 {
-	va_list args;
-	int i = 0;
 	int len = 0;
+	va_list args;
 
 	va_start(args, str);
 	while (str && *str)
@@ -89,13 +83,20 @@ int ft_printf(const char *str, ...)
 		str++;
 	}
 	va_end(args);
-	return(len);
+	return (len);
 }
 
 #include <stdio.h>
 int main()
 {
-	ft_putstr("ceci est un test");
-	printf("%d\n", ft_puthex(15));
+	//ft_putstr("ceci est un test");
+	//ft_puthex(42);
+	//printf("%d\n", ft_puthex(42));
+	char *string = "hello";
+	int n = 42;
+	//ft_printf("test: %s", test);
+	ft_printf("lenght ft_printf: %d\n", ft_printf("test: %x\n", n));
+	printf("lenght printf: %d\n", printf("test: %x\n", n));
+
 	return (0);
 }
